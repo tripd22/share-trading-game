79143d4ed7e6a02b2f1204c4eb0805eb68d20b60
tripd22 <42713201+tripd22@users.noreply.github.com>
2019-01-21 23:46:24 +1100

    django serving game as index

--- sharetrading/game/views.py
+++ sharetrading/game/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello, world. You are at the game")
+    template = loader.get_template('game/index.html')
+    context = {
+        'num': 2,
+    }
+    return HttpResponse(template.render(context, request))
