f2b65356d57dfa5df1b760a94806d626b424235d
tripd22 <42713201+tripd22@users.noreply.github.com>
2019-02-21 22:35:21 +1100

    added login functionality

--- sharetrading/sharetrading/urls.py
+++ sharetrading/sharetrading/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+		path('accounts/', include('django.contrib.auth.urls')),
         path('game/', include('game.urls')),
         path('admin/', admin.site.urls),
 ]
